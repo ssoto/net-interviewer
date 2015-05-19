e31c97e9e7fb113bd5035a90d20b81edac3b43b3
Sergio Soto <ssoto@wtelecom.es>
2015-05-19 09:58:15 +0200

    Added argparse to make use with arguments

--- src/snmp_table.py
+++ src/snmp_table.py
@@ -1,46 +1,64 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
 from snmp.table import Snmp_table_request
+from sys import argv
 from pprint import pprint
 
 from utils.ip_manager import get_decimal_ip
 
 
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-p', '--port', dest='port',
+                        default='161', action='store',
+                        help='port to send snmp command')
+
+    parser.add_argument('-s', '--server', dest='server', required=True,
+                        help='server to send snmp command')
+
+    parser.add_argument('-c', '--community', dest='community', required=True,
+                        help='snmp community field')
+
+    parser.add_argument('-sv', '--snmpversion', dest='snmp_version', default='2c',
+                        help='snmp version')
+
+    parser.add_argument('-m', '--mib-name', dest='mib_name', required=True,
+                        help='mib name')
+
+    parser.add_argument('-o', '--oids', required=True,
+                        type=str, nargs='+',
+                        dest='oids',  help='list of oids')
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
 
-    ip = '10.200.0.10'
-    community = 'wtelecom'
-    version = '2c'
-    port = 161
-    cisco_DOT11 = 'CISCO-DOT11-ASSOCIATION-MIB'
-    config_table_OID = 'cDot11ClientConfigInfoTable'
-    statistic_table = 'cDot11ClientStatisticTable'
+    args = parse_args()
 
+    for oid in args.oids:
 
-    config_table_OID_req = Snmp_table_request(server = ip, 
-                                   community = community, 
-                                   snmp_version = version,
-                                   port = port,
-                                   mib_name = cisco_DOT11, 
-                                   oid_name = config_table_OID)
-    config_table_OID_req.request()
-    config_table_dict = config_table_OID_req.get_json_reply()
+        OID_req = Snmp_table_request(server = args.server, 
+                                       community = args.community, 
+                                       snmp_version = args.snmp_version,
+                                       port = args.port,
+                                       mib_name = args.mib_name, 
+                                       oid_name = oid)
+        OID_req.request()
+        table_dict = OID_req.get_json_reply()
+        
+        if table_dict[0].has_key('IpAddress'):
+            for index in table_dict:
+                element = table_dict[index]
+                element['IpAddress'] = get_decimal_ip(element['IpAddress'])
+
+        print args.mib_name+"::"+oid
+        pprint (table_dict)
+
+        print ('===================================================')
+
     
-    for index in config_table_dict:
-        element = config_table_dict[index]
-        element['IpAddress'] = get_decimal_ip(element['IpAddress'])
-
-    print ('===================================================')
-
-    statistic_table_OID_req = Snmp_table_request(server = ip, 
-                                   community = community, 
-                                   snmp_version = version,
-                                   port = port,
-                                   mib_name = cisco_DOT11, 
-                                   oid_name = statistic_table)
-    statistic_table_OID_req.request()
-
-    statistic_dict = statistic_table_OID_req.get_json_reply()
-    
-    import ipdb; ipdb.set_trace()
