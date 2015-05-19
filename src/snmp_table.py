993c8935416b730d4b135d3cb7a4bd44062817c0
Sergio Soto <ssoto@wtelecom.es>
2015-05-19 12:54:31 +0200

    Command line features added. Added field addiction between json

--- src/snmp_table.py
+++ src/snmp_table.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import json
+import os
+from pprint import pprint
 from snmp.table import Snmp_table_request
 from sys import argv
-from pprint import pprint
 
-from utils.ip_manager import get_decimal_ip
+from utils.ip_manager import get_decimal_ip, append_field
+from utils.color_print import error, success
 
 
 def parse_args():
@@ -23,8 +26,8 @@ def parse_args():
     parser.add_argument('-c', '--community', dest='community', required=True,
                         help='snmp community field')
 
-    parser.add_argument('-sv', '--snmpversion', dest='snmp_version', default='2c',
-                        help='snmp version')
+    parser.add_argument('-sv', '--snmpversion', dest='snmp_version', 
+                        default='2c', help='snmp version')
 
     parser.add_argument('-m', '--mib-name', dest='mib_name', required=True,
                         help='mib name')
@@ -33,32 +36,65 @@ def parse_args():
                         type=str, nargs='+',
                         dest='oids',  help='list of oids')
 
-    return parser.parse_args()
+    parser.add_argument('-w', '--write_output', dest='file_path',
+                        help='if passed, snmp output are writed on the file')
+    
+    args_parsed = parser.parse_args()
 
+    # check file to output to print
+    if hasattr(args_parsed, 'file_path') and os.path.isfile(args_parsed.file_path):
+        try: 
+            open(args_parsed.file_path)
+        except IOError as e:
+            error("Error with file to write request output: " + args_parsed.file_path)
+            sys.exit(0)
+    
+    return args_parsed
+
+
+def write_on_file (file_name, object_data):
+    with open(file_name+'.json', 'w') as outfile:
+        json.dump(object_data, outfile, indent=2)
 
 if __name__ == '__main__':
 
     args = parse_args()
+    data_dict = {}
 
     for oid in args.oids:
 
-        OID_req = Snmp_table_request(server = args.server, 
-                                       community = args.community, 
-                                       snmp_version = args.snmp_version,
-                                       port = args.port,
-                                       mib_name = args.mib_name, 
-                                       oid_name = oid)
-        OID_req.request()
+        OID_req = Snmp_table_request( server = args.server, 
+                                     community = args.community, 
+                                     snmp_version = args.snmp_version,
+                                     port = args.port,
+                                     mib_name = args.mib_name, 
+                                     oid_name = oid)
+        try:
+            OID_req.request()
+        except Exception as excep:
+            error("Error on request " + args.mib_name + '::' + oid )
+            break
         table_dict = OID_req.get_json_reply()
         
-        if table_dict[0].has_key('IpAddress'):
+        if oid == 'cDot11ClientConfigInfoTable':
             for index in table_dict:
                 element = table_dict[index]
                 element['IpAddress'] = get_decimal_ip(element['IpAddress'])
+        
+        mib_oid_name = args.mib_name+"::"+oid 
+        
+        data_dict[mib_oid_name] = table_dict
+        write_on_file(mib_oid_name, table_dict)
 
-        print args.mib_name+"::"+oid
-        pprint (table_dict)
+    # add ip field from 
+    append_field(data=data_dict, 
+                 original_ds='CISCO-DOT11-ASSOCIATION-MIB::cDot11ClientConfigInfoTable',
+                 destination_ds='CISCO-DOT11-ASSOCIATION-MIB::cDot11ClientStatisticTable',
+                 field='IpAddress')
 
-        print ('===================================================')
+    output_file = "./output_test"
+    success("Request done successfully, output json writed on " + args.file_path)
+    write_on_file( file_name="./output_test", 
+                   object_data = data_dict["CISCO-DOT11-ASSOCIATION-MIB::cDot11ClientStatisticTable"])
 
     
--- src/utils/ip_manager.py
+++ src/utils/ip_manager.py
@@ -5,3 +5,44 @@
 def get_decimal_ip(hexadecimal_ip):
         decimal_ip_array = [ str(int('0x'+hex,0)) for hex in hexadecimal_ip.split(' ')]
         return '.'.join(decimal_ip_array)
+
+def append_field(data, original_ds, destination_ds, field):
+    """
+    Try add a new field on destination_ds for each element in 
+    destination_ds with same key. 
+
+    >>> print data
+    data  = {
+        original_ds : {
+            key_a : {
+                target_field: value
+                ...
+            }
+        },
+        destination_ds : {
+            key_a : {
+                ...
+            }
+        } 
+    }
+    >>> append_field (data, "original_ds", "destination_ds", "target_field") 
+    >>> print data
+    data  = {
+        original_ds : {
+            key_a : {
+                target_field: value
+                ...
+            }
+        },
+        destination_ds : {
+            key_a : {
+                target_field: value
+                ...
+            }
+        } 
+    }
+
+    """
+    for ident in data[destination_ds].keys():
+        ip_address = data[original_ds][ident][field]
+        data[destination_ds][ident][field] = ip_address
