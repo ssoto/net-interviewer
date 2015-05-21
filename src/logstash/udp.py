bb4c824a667eeccb868cec40e3234654127af3fc
Sergio Soto <ssoto@wtelecom.es>
2015-05-21 18:35:47 +0200

    Added sub operation to enable metrics in register that are incremental

--- src/logstash/udp.py
+++ src/logstash/udp.py
@@ -3,24 +3,62 @@
 
 import json
 import logging
+import re
 import socket
-
 
 class Sender():
 
     def __init__(self, server, port):
         self.__ip = server
         self.__port = port
+        self.__memory_dict = None
 
-    def send_dict(self, dict):
-        self.send_str(json.dumps(dict))
+    def send_dict_values(self, dict_2_send, register_fields=None):
 
-    def send_str(self, json_str):
-        sock = socket.socket(socket.AF_INET, # Internet
-                             socket.SOCK_DGRAM) # UDP
-        sock.sendto(json_str, 
-                    (self.__ip, self.__port))
+        if register_fields and len(register_fields):
+                        
+            original = dict_2_send
 
+            if self.__memory_dict:
+                final_dict = self.__update_registers_fields(dict_2_send, register_fields)
+                self.__iterate_and_send(final_dict)
+            
+            self.__memory_dict = original
+        else:
+            # dict "sotred" is the original, without managing register fields
+            self.__iterate_and_send(dict_2_send)
+
+    def __iterate_and_send(self, dict_2_send):
+        for element in dict_2_send:
+            dict_str = json.dumps(dict_2_send[element])
+            sock = socket.socket(socket.AF_INET, # Internet
+                                 socket.SOCK_DGRAM) # UDP
+            sock.sendto(dict_str, 
+                        (self.__ip, self.__port))
+
+    def __update_registers_fields(self, dictionary, fields):
+        # mac identifier level
+        for element_key in dictionary:
+
+            # I get only values for fields list
+            new_values = {k: v for k, v in dictionary[element_key].items() if k in fields}
+            try:
+                old_values = {k: v for k, v in (self.__memory_dict[element_key]).items() if k in fields}
+            except KeyError as e:
+                logging.info("New device: " + element_key)
+                break
+            for field in fields:
+                if new_values[field] and old_values[field]:
+                    new_ammount = int(re.findall(r'\d+',new_values[field])[0])
+                    old_ammount = int(re.findall(r'\d+',old_values[field])[0])
+
+                    dictionary[element_key][field+'Diff'] = abs(old_ammount - new_ammount)
+                else:
+                    pass
+            #import ipdb; ipdb.set_trace()
+        return dictionary
+            
+        
 if __name__ == "__main__":
     obj = {
         "14:30:c6:bb:ef:14": {
