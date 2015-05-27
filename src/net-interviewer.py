6f97655c9078cce4dc5f89c016fdcebda4026105
Sergio Soto <ssoto@wtelecom.es>
2015-05-27 18:42:18 +0200

    util.thread.StoppableThread works and can be stopped

--- src/net-interviewer.py
+++ src/net-interviewer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from threading import Timer
+import threading
 import time
 from utils.config_parser import get_config
+from Queue import Queue
 
 import logging
 logging.basicConfig( format='%(asctime)s - %(pathname)s:%(lineno)d : %(levelname)s  : %(message)s', 
@@ -14,21 +15,32 @@ class SNMP_table_job:
     def __init__(self, name,host,port,snmp_community,mib,oids, interval):
         arguments = locals()
         self.name = name
-        self.interval = interval
+        self.interval = int(interval)
+
+    def start(self):
+
+        self.__timer = threading.Timer(self.interval, self.task).start()
+
+    def stop(self):
+        if hasattr(self, "__timer") and self.__timer:
+            self.__timer.stop()
 
     def __str__(self):
         import pprint
         pprint.pprint(self.attributes)
 
-    def run(self):
+
+    def task(self):
         logging.debug("Task " + self.name + " is working!")
+        self.__timer = threading.Timer(self.interval, self.task).start()
+
 
 if __name__ == "__main__":
 
     config_dict = get_config('./net_interviewer.conf')
 
     devices_config =  { k:v for (k,v) 
-            in config_dict.iteritems() 
+            in config_dict.iteritems()
             if "Daemon-device" in k}
 
     task_list = []
@@ -43,21 +55,18 @@ if __name__ == "__main__":
                               oids=[item.strip() for item in config['oids'].split(',')],
                               interval=int(config['interval']))
 
-        timer = Timer( int(config['interval']), 
-                       task.run, 
-                       ())
-
         logging.debug("created job %s  with interval %s" %(task.name, task.interval ))
 
-        task_list.append([task, timer])
-        timer.start()
+        task_list.append(task)
+        task.start()
 
     try:
         while True:
             time.sleep(0.5)
     except KeyboardInterrupt:
-        for (task, timer) in task_list:
-            timer.cancel()
+        logging.debug("trying exit...")
+        for task in task_list:
+            task.stop()
             logging.debug("stopping job %s  with interval %s" %(task.name, task.interval ))
 
 
