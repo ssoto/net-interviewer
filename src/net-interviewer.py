1f0982ac33a724778b3422dc31dad4f646e8625d
Sergio Soto <ssoto@wtelecom.es>
2015-05-28 17:37:46 +0200

    Fixed config file parsing

--- src/net-interviewer.py
+++ src/net-interviewer.py
@@ -14,6 +14,29 @@ logging.basicConfig(
     format='%(asctime)s - %(pathname)s:%(lineno)d : %(levelname)s  : %(message)s', 
     level=logging.DEBUG)
 
+def generate_config(config_file_path):
+
+    config_dict = get_config(config_file_path)
+    
+    devices_config =  { k:v for (k,v) 
+            in config_dict.iteritems() if "Daemon-device" in k}
+
+    requests = []
+
+    for config in  devices_config.values():
+        for oid in [item.strip() for item in config['oids'].split(',')]:
+            for server in [item.strip() for item in config['device'].split(',')]:
+                kwargs = {
+                    'oid': oid,
+                    'server': server,
+                    'community': config['community'],
+                    'port': config['port'],
+                    'mib_name': config['mib']
+                }
+                requests.append(kwargs)
+    return (config, requests)
+
+
 def task( server, community, port, mib_name, oid):
 
     OID_req = Snmp_table_request( server=server, mib_name=mib_name, community=community,oid=oid, port=port)
@@ -28,42 +51,28 @@ def task( server, community, port, mib_name, oid):
 
 if __name__ == "__main__":
 
-    config_dict = get_config('./net_interviewer.conf')
-
-    devices_config =  { k:v for (k,v) 
-            in config_dict.iteritems() if "Daemon-device" in k}
+    (config, requests) = generate_config('./net_interviewer.conf')
 
     data_manager = DataManager()
     task_list = []
+    
+    for request in requests:
+        th = StoppableThread(interval=config['interval'], 
+                             target=task,
+                             kwargs=request)
 
-    for config in  devices_config.values():
-        for oid in [item.strip() for item in config['oids'].split(',')]:
-            for server in [item.strip() for item in config['device'].split(',')]:
-            
-                try:
-                    kwargs = {
-                        'oid': oid,
-                        'server': server,
-                        'community': config['community'],
-                        'port': config['port'],
-                        'mib_name': config['mib']
-                    }
-                    th = StoppableThread(interval=config['interval'], 
-                                         target=task,
-                                         kwargs=kwargs)
-                    logging.debug("created new thread to process %s::%s to %s" 
-                        %(config['mib'], oid, server))
-                    th.start()
-                except Exception as e:
-                    logging.error("error creating tasks", repr(e))
-                else:
-                    task_list.append(th)
+        logging.debug("created new thread to process %s::%s to %s" 
+            %(request['mib_name'], request['oid'], request['server']))
+        th.start()
+        task_list.append(th)
 
     try:
         while True:
             time.sleep(0.5)
     except KeyboardInterrupt:
+        
         logging.debug("trying exit...")
+
         for task in task_list:
             task.stop()
             logging.debug("stopping job %s  with interval %s"
